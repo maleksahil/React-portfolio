b0c21bee7b16c684ce313a8ea58cd633e94150d2
sahil <maleksahil9315@gmail.com>
2025-06-07 12:47:32 +0530

    portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+const Header = () => {
+  return (
+    <div className='fixed w-full z-10 p-20 flex items-center justify-end'>
+      <button className='bg-blue-400 hover:bg-blue-200 cursor-pointer hover:text-black shadow-xl duration-1700 px-6 py-3 rounded-full text-xl text-white border-4'>Hire me</button>
+      <i className="ri-menu-line text-4xl ml-3"></i>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import me from '../images/me.png'
+import backgroundImg from '../images/hand-painted-watercolor-background-with-sky-clouds-shape.jpg'
 
 const Page1 = () => {
   return (
-    <div className='bg-pink-300 h-screen'>
-   
+    <div className='bg-white h-screen p-7'>
+       <div className='shadow-xl shadow-gray-700 h-full w-full rounded-3xl' style={{ backgroundImage: `url(${backgroundImg})` }}>
+       <img className='absolute right-20 bottom-25 transform scale-125' src={me}/>
+       </div>
+
     </div>
   )
 }
